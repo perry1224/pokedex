8592c74f8f19f574c7445890c110c7b8cde022f0
perry1224 <perry.w1224@gmail.com>
2022-04-29 21:16:10 -0400

    change to height

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-let pokemonList = [ {name: "bulbasaur", type: "grass", weight:"30lbs"}, 
-{name: "squirtle", type:"water", weight: "20lbs"}, {name: "charmander", type: "fire", weight: "15lbs"}];
+let pokemonList = [ {name: "bulbasaur", type: "grass", height:"5"}, 
+{name: "squirtle", type:"water", height: "7"}, {name: "charmander", type: "fire", height: "9"}];
 document.write(pokemonList);
