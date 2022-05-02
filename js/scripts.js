30e8631bbda9e0f4128e8a5102d59b8d66bd8024
perry1224 <perry.w1224@gmail.com>
2022-05-01 21:29:48 -0400

    added bg color

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-let pokemonList = [ {name: "bulbasaur", type: "grass", height:"5"}, 
-{name: "squirtle", type:"water", height: "7"}, {name: "charmander", type: "fire", height: "9"}];
-document.write(pokemonList);
+
+let pokemonList = [ 
+{name: "bulbasaur", type: ["grass", "poison"], height:"5"}, 
+{name: "squirtle", type:["water"], height: "7"}, 
+{name: "charmander", type: ["fire"], height: "9"}
+];
+
+for (let i=0; i<=pokemonList.length; i++) {
+if (pokemonList[i].height > 7) {
+    
+
+    document.write(pokemonList[i].name +' ' + pokemonList[i].type + '(height:' + pokemonList[i].height + ')' + 'Wow you are tall!' + '<BR>');
+} else {
+    document.write(pokemonList[i].name + ' ' + pokemonList[i].type + '(height:' + pokemonList[i].height + ')'  + '<BR>');
+}
+
+
+}
