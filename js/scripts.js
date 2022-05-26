e5a8fe8644f27ec90346fcf52df868d0c5044964
perry1224 <perry.w1224@gmail.com>
2022-05-25 22:39:46 -0400

    removed modalcontainer

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-let modalContainer = document.querySelector('#modal-container');
+
 let input = $('input');
 input.on('input', filterList);  
   
